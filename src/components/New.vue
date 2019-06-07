3313931b6526c955c7ebb473bde781d425de5a05
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-06-07 14:47:18 -0300

    Add files via upload

---
+++ src/components/New.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h3>The New</h3>
+        <button @click="counter++">Increase!</button>
+        <p>{{ counter }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function(){
+            return {
+                counter: 0
+            };
+        },
+        destroyed(){
+            console.log('Destroyed!');
+        },
+        deactivated(){
+            console.log('Deactivated!');
+        },
+        activated(){
+            console.log('Activated!');
+        }
+    }
+</script>
+
+<style>
+
+</style>
